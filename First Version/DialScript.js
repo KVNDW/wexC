b53bd0e21a8e9d2a648c05366862b23ddf257ab5
carolin <carolin.maeder@students.fhnw.ch>
2024-03-06 14:35:28 +0100

    added moon

--- First Version/DialScript.js
+++ First Version/DialScript.js
@@ -1,6 +1,7 @@
 const slider1 = document.getElementById('slider1');
 const display1 = document.getElementById('display1');
 const sun = document.getElementById('sun');
+const moon = document.getElementById('moon');
 const sky = document.getElementById('sky');
 
 
@@ -8,20 +9,33 @@ const updateValue = () => {
     display1.textContent = slider1.value;
 
     updateSunPosition();
+    updateMoonPosition();
     updateSkyColor();
 }
 
 const updateSunPosition = () => {
-    const angle = (slider1.value / slider1.max) * Math.PI - Math.PI / 2;
+    const angle = (slider1.value / slider1.max) * Math.PI*2 - Math.PI / 2 ;
     const centerXArch = 200;
-    const centerYArch = 160;
-    const radiusXArch = 230;
-    const radiusYArch = 130;
+    const centerYArch = 230;
+    const radiusXArch = 180;
+    const radiusYArch = 180;
     const xPos = centerXArch + radiusXArch * Math.sin(angle);
     const yPos = centerYArch - radiusYArch * Math.cos(angle);
 
     sun.setAttribute("cx", xPos);
     sun.setAttribute("cy", yPos);
+}
+const updateMoonPosition = () => {
+    const angle = ((slider1.value / slider1.max) * Math.PI*2 + Math.PI/2 );
+    const centerXArch = 200;
+    const centerYArch = 230;
+    const radiusXArch = 180;
+    const radiusYArch = 180;
+    const xPos = centerXArch + radiusXArch * Math.sin(angle);
+    const yPos = centerYArch - radiusYArch * Math.cos(angle);
+
+    moon.setAttribute("cx", xPos);
+    moon.setAttribute("cy", yPos);
 }
 
 const updateSkyColor=()=>{
